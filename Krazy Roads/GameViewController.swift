d1ca97dde1c8d208d92f38cf01a5a8205bcedd63
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-10-08 15:37:10 -0600

    adds camera and floor nodes

--- Krazy Roads/GameViewController.swift
+++ Krazy Roads/GameViewController.swift
@@ -14,11 +14,13 @@ class GameViewController: UIViewController {
     
     var scene: SCNScene!
     var sceneView: SCNView!
+    var cameraNode = SCNNode()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupScene()
-        
+        setupFloor()
+        setupCamera()
     }
     
     func setupScene() {
@@ -27,4 +29,23 @@ class GameViewController: UIViewController {
         
         sceneView.scene = scene
     }
+    
+    func setupFloor() {
+        let floor = SCNFloor()
+        floor.firstMaterial?.diffuse.contents = UIColor.green
+        floor.reflectivity = 0.0
+        
+        let floorNode = SCNNode(geometry: floor)
+        scene.rootNode.addChildNode(floorNode)
+    }
+    
+    
+    func setupCamera() {
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 10, z: 0)
+        cameraNode.eulerAngles = SCNVector3(x: -.pi/2, y: 0, z: 0)
+        scene.rootNode.addChildNode(cameraNode)
+    }
+    
+    
 }
