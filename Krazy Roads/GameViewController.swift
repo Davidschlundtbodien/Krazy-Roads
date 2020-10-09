11283c93ac0f76795804e7b88d63bfe88e8d8d7b
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-10-09 15:08:39 -0600

    adds random road and grass lane generation

--- Krazy Roads/GameViewController.swift
+++ Krazy Roads/GameViewController.swift
@@ -15,6 +15,9 @@ class GameViewController: UIViewController {
     var scene: SCNScene!
     var sceneView: SCNView!
     var cameraNode = SCNNode()
+    var mapNode = SCNNode()
+    var lanes = [LaneNode]()
+    var laneCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,11 +31,25 @@ class GameViewController: UIViewController {
         scene = SCNScene()
         
         sceneView.scene = scene
+        
+        scene.rootNode.addChildNode(mapNode)
+        
+        for _ in 0..<20 {
+            let type = randomBool(odds: 3) ? LaneType.grass : LaneType.road
+            let lane = LaneNode(type: type, width: 21)
+            lane.position = SCNVector3(0, 0, 5 - Float(laneCount))
+            laneCount += 1
+            lanes.append(lane)
+            mapNode.addChildNode(lane)
+        }
     }
     
     func setupFloor() {
         let floor = SCNFloor()
-        floor.firstMaterial?.diffuse.contents = UIColor.green
+        floor.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/darkgrass.png")
+        floor.firstMaterial?.diffuse.wrapS = .repeat
+        floor.firstMaterial?.diffuse.wrapT = .repeat
+        floor.firstMaterial?.diffuse.contentsTransform = SCNMatrix4MakeScale(12.5, 12.5, 12.5)
         floor.reflectivity = 0.0
         
         let floorNode = SCNNode(geometry: floor)
@@ -43,7 +60,7 @@ class GameViewController: UIViewController {
     func setupCamera() {
         cameraNode.camera = SCNCamera()
         cameraNode.position = SCNVector3(x: 0, y: 10, z: 0)
-        cameraNode.eulerAngles = SCNVector3(x: -.pi/2, y: 0, z: 0)
+        cameraNode.eulerAngles = SCNVector3(x: -toRadians(angle: 72), y: toRadians(angle: 9), z: 0)
         scene.rootNode.addChildNode(cameraNode)
     }
     
---
+++ Krazy Roads/Utils.swift
@@ -0,0 +1,30 @@
+//
+//  Utils.swift
+//  Krazy Roads
+//
+//  Created by David Schlundt-Bodien on 10/9/20.
+//  Copyright © 2020 David Schlundt-Bodien. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+let degreesPerRadians = Float(Double.pi/180)
+let radiansPerDegrees = Float(180/Double.pi)
+
+func toRadians(angle: Float) -> Float {
+    return angle * degreesPerRadians
+}
+
+func toRadians(angle: CGFloat) -> CGFloat {
+    return angle * CGFloat(degreesPerRadians)
+}
+
+func randomBool(odds: Int) -> Bool {
+    let random = arc4random_uniform(UInt32(odds))
+    if random < 1 {
+        return true
+    } else {
+        return false
+    }
+}
